8c0b5607ba527ac7ae8407165c3ff351703192ac
N D <detroitmuseumofhealing@gmail.com>
2017-11-13 09:40:26 -0500

    next prev flip working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
 // This will be the values we start out with when the page loads.
 const INITIAL_STATE = {
-    isFront: true,
-    currentCardIndex: 0,
-    cards: [
-        { front: "A front", back: "A back" },
-        { front: "B front", back: "B back" },
-        { front: "C front", back: "C back" },
-        { front: "D front", back: "D back" },
-        { front: "E front", back: "E back" },
-        { front: "F front", back: "F back" }
-    ]
+  isFront: true,
+  currentCardIndex: 0,
+  cards: [
+    { front: "A front", back: "A back" },
+    { front: "B front", back: "B back" },
+    { front: "C front", back: "C back" },
+    { front: "D front", back: "D back" },
+    { front: "E front", back: "E back" },
+    { front: "F front", back: "F back" }
+  ]
 }
 
 // This one function is the reducer. The reducer is the heart of Redux. It tells
@@ -18,29 +18,33 @@ const INITIAL_STATE = {
 // We usually specify the initial state as the ES6 default value for the state
 // parameter.
 export default function(state = INITIAL_STATE, action) {
-    switch (action.type) {
-        case "NEXT":
-            return Object.assign({}, state, {
-                currentCardIndex: state.currentCardIndex + 1
-            });
+  switch (action.type) {
+    case "NEXT":
+    return Object.assign({}, state, {
+      currentCardIndex: state.currentCardIndex + 1
+    });
 
-            case "PREVIOUS":
-                return Object.assign({}, state, {
-                    currentCardIndex: state.currentCardIndex - 1
-                });
+    case "PREVIOUS":
+    return Object.assign({}, state, {
+      currentCardIndex: state.currentCardIndex - 1
+    });
 
-              case "FLIP":
-                return Object.assign({}, state, {
-                     isFront: state.isFront !== true
-                   });
+    case "FLIP":
+    return Object.assign({}, state, {
+      isFront: state.isFront !== true
+    });
 
-
-                //case "SHUFFLE":
-                  //return Object.assign({}, state, {
-                   //default:
-                   //return state;
-                      //  });
+    default:
+    return state;
 
 
-    }
+    
+    //case "SHUFFLE":
+    //return Object.assign({}, state, {
+    //default:
+    //return state;
+    //  });
+
+
+  }
 }
